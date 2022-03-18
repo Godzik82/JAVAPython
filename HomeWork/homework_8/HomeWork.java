6ee77b72d81988cb80857176475968f3e37f4594
Godzik82 <godzik82@mail.ru>
2022-03-19 00:14:41 +0400

    Add homework_8

--- HomeWork/homework_8/HomeWork.java
+++ HomeWork/homework_8/HomeWork.java
@@ -4,12 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class HomeWork {
-    public static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    public static Integer check;
-    public static Map<Product, Integer> store = new HashMap<>();
+    public static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    protected static Integer check;
+    protected static final List<Map<Product, Integer>> store = new LinkedList<>();
 
     public static void main (String[] args) throws IOException {
         
@@ -30,26 +32,32 @@ public class HomeWork {
     }
 
     public static Integer mainMenu() throws IOException {
-        System.out.println("____МЕНЮ____\n1. Добавить товар\n2. Удалить товар\n3. Вывести все товары\n4. Выход");
+        System.out.println("____МЕНЮ____\n1. Добавить товар\n2. Удалить товар\n3. Вывести все товары\n4. Выход\nВыберите пункт:");
         return  Integer.parseInt(reader.readLine());
     }
 
     public static void putStore() throws IOException {
         System.out.println("Введите через пробел название товара, его стоимость, количество:");
         String[] infoProduct = reader.readLine().split(" ");
-        store.put(new Product(infoProduct[0],
-                                Double.parseDouble(infoProduct[1])),
-                                Integer.parseInt(infoProduct[2]));
+        Map <Product, Integer> newProduct = createProduct(infoProduct);
+        System.out.println(addProduct(newProduct));
     }
 
     public static void removeFromStore() throws IOException {
         infoStore();
-        if (store.size() != 0)
+        if (store.isEmpty()){
             System.out.println("Введите через пробел название и цену товара, который будет удален:");
             String[] infoRemoveProduct = reader.readLine().split(" ");
-            
-
-        
+            Product removeProduct = new Product(infoRemoveProduct[0], Float.parseFloat(infoRemoveProduct[1]));
+            for (Integer i = 0; i < store.size(); i++){
+                System.out.println(store.get(i).keySet().hashCode() + " " + removeProduct.hashCode());
+                if (store.get(i).keySet().hashCode() == removeProduct.hashCode()){
+                    store.remove(store.get(i));
+                    System.out.println("Товар удален");
+                    break;
+                }
+            }
+        }                      
     }
     
     public static void infoStore(){
@@ -57,9 +65,32 @@ public class HomeWork {
             System.out.println("\nНа складе нет товаров\n");
         else {
             System.out.println("На складе находятся следующие товары\nНазвание\tСтоимость\tКоличество");
-            for (Map.Entry<Product, Integer> product: store.entrySet())
-                System.out.printf("%s\t%9d%n%n", product.getKey().info(), product.getValue());
-            }
+            for (Map <Product, Integer> product : store)
+                for (Map.Entry<Product, Integer> p : product.entrySet())
+                    System.out.printf("%-16s%-16.2f%-16d%n",
+                                            p.getKey().name,
+                                            p.getKey().cost,
+                                            product.get(p.getKey()));
+            System.out.println("----------END----------\n");
         }
+    }
+
+    public static Map<Product, Integer> createProduct(String[] info){
+        Map<Product, Integer> product = new HashMap<>();
+        product.put(new Product(info[0], Float.parseFloat(info[1])), Integer.parseInt(info[2]));
+        return product;
+    }
+    
+    public static String addProduct(Map <Product, Integer> n){
+        for(Integer i = 0; i < store.size(); i++)
+            if (store.get(i).keySet().hashCode() == n.keySet().hashCode()){
+                Integer newValue = store.get(i).get(store.get(i).keySet().toArray()[0]) +
+                                    n.get(n.keySet().toArray()[0]);
+                store.get(i).replace((Product) store.get(i).keySet().toArray()[0], newValue);
+                return "Количество товара " + ((Product) n.keySet().toArray()[0]).getName() + " увеличено";
+            }
+        store.add(n);
+        return "Добавлен товар - " + ((Product) n.keySet().toArray()[0]).getName();
+    }
 }
 
--- HomeWork/homework_8/Product.java
+++ HomeWork/homework_8/Product.java
@@ -2,22 +2,42 @@ package homework_8;
 
 public class Product implements Comparable<Product>{
     String name;
-    Double cost;
+    Float cost;
 
-    public Product(String name, Double cost){
+    public Product(String name, Float cost){
         this.name = name;
         this.cost = cost;
     }
 
-    public String info(){
-        return name + "         \t" + cost;
+    @Override
+    public int compareTo(Product o) {
+        if (name.equals(o.name) && cost.equals(o.cost))
+            return 0;
+        else return 1;
     }
 
     @Override
-    public int compareTo(Product o) {
-        if (name.equals(o.name))
-            return 0;
-        else return 1;
+    public boolean equals(Object o){
+        if (o instanceof Product){
+            Product p = (Product) o;
+            return name == p.name && cost == p.cost;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode(){
+        int code = name == null ? 0 : name.hashCode();
+        code += cost;
+        return code;
+    }
+
+    public String getName(){
+        return name;
+    }
+    
+    public Float getCost(){
+        return cost;
     }
     
 }
