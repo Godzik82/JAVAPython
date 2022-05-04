a7333ecad0bedaeb2bc439c6de2fb537018081a7
Godzik82 <godzik82@mail.ru>
2022-05-04 23:33:49 +0400

    Add homework 14

---
+++ HomeWork/homework_14/homework.sql
@@ -0,0 +1,20 @@
+-- SQLite
+CREATE TABLE users (
+    name CHAR,
+    surname CHAR,
+    money INTEGER,
+    birthday DATETIME
+);
+
+CREATE TABLE cars (
+    name CHAR,
+    color CHAR,
+    model CHAR
+);
+
+CREATE TABLE shops (
+    name CHAR,
+    address CHAR
+);
+
+
